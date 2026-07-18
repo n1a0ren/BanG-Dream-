3dd176a8825298721ef4056108917a119d85323d
n1a0ren <niaorennb@gmail.com>
2026-07-19 00:12:26 +0800

    7/19/2026

--- src/com/game/Characters.java
+++ src/com/game/Characters.java
@@ -1,5 +1,7 @@
 package com.game;
 
+import com.game.ui.App;
+
 import javax.swing.*;
 import java.util.ArrayList;
 import java.util.List;
@@ -8,63 +10,63 @@ public class Characters {
         //Creating image-icon
 
         //Poppin' Party
-        public static final ImageIcon Kasumi = new ImageIcon("C:\\JavaGame\\src\\com\\game\\resources\\Kasumi.png");
-        public static final ImageIcon Tae = new ImageIcon("C:\\JavaGame\\src\\com\\game\\resources\\Tae.png");
-        public static final ImageIcon Rimi = new ImageIcon("C:\\JavaGame\\src\\com\\game\\resources\\Rimi.png");
-        public static final ImageIcon Saya = new ImageIcon("C:\\JavaGame\\src\\com\\game\\resources\\Saya.png");
-        public static final ImageIcon Arisa = new ImageIcon("C:\\JavaGame\\src\\com\\game\\resources\\Arisa.png");
+        public static final ImageIcon Kasumi = new ImageIcon("src/com/game/resources/Kasumi.png");
+        public static final ImageIcon Tae = new ImageIcon("src/com/game/resources/Tae.png");
+        public static final ImageIcon Rimi = new ImageIcon("src/com/game/resources/Rimi.png");
+        public static final ImageIcon Saya = new ImageIcon("src/com/game/resources/Saya.png");
+        public static final ImageIcon Arisa = new ImageIcon("src/com/game/resources/Arisa.png");
 
         //Afterglow
-        public static final ImageIcon Ran = new ImageIcon("C:\\JavaGame\\src\\com\\game\\resources\\Ran.png");
-        public static final ImageIcon Moca = new ImageIcon("C:\\JavaGame\\src\\com\\game\\resources\\Moca.png");
-        public static final ImageIcon Himari = new ImageIcon("C:\\JavaGame\\src\\com\\game\\resources\\Himari.png");
-        public static final ImageIcon Tomoe = new ImageIcon("C:\\JavaGame\\src\\com\\game\\resources\\Tomoe.png");
-        public static final ImageIcon Tsugumi = new ImageIcon("C:\\JavaGame\\src\\com\\game\\resources\\Tsugumi.png");
+        public static final ImageIcon Ran = new ImageIcon("src/com/game/resources/Ran.png");
+        public static final ImageIcon Moca = new ImageIcon("src/com/game/resources/Moca.png");
+        public static final ImageIcon Himari = new ImageIcon("src/com/game/resources/Himari.png");
+        public static final ImageIcon Tomoe = new ImageIcon("src/com/game/resources/Tomoe.png");
+        public static final ImageIcon Tsugumi = new ImageIcon("src/com/game/resources/Tsugumi.png");
 
         //Pastel*Palettes
-        public static final ImageIcon Aya = new ImageIcon("C:\\JavaGame\\src\\com\\game\\resources\\Aya.png");
-        public static final ImageIcon Hina = new ImageIcon("C:\\JavaGame\\src\\com\\game\\resources\\Hina.png");
-        public static final ImageIcon Chisato = new ImageIcon("C:\\JavaGame\\src\\com\\game\\resources\\Chisato.png");
-        public static final ImageIcon Maya = new ImageIcon("C:\\JavaGame\\src\\com\\game\\resources\\Maya.png");
-        public static final ImageIcon Eve = new ImageIcon("C:\\JavaGame\\src\\com\\game\\resources\\Eve.png");
+        public static final ImageIcon Aya = new ImageIcon("src/com/game/resources/Aya.png");
+        public static final ImageIcon Hina = new ImageIcon("src/com/game/resources/Hina.png");
+        public static final ImageIcon Chisato = new ImageIcon("src/com/game/resources/Chisato.png");
+        public static final ImageIcon Maya = new ImageIcon("src/com/game/resources/Maya.png");
+        public static final ImageIcon Eve = new ImageIcon("src/com/game/resources/Eve.png");
 
         //Roselia
-        public static final ImageIcon Yukina = new ImageIcon("C:\\JavaGame\\src\\com\\game\\resources\\Yukina.png");
-        public static final ImageIcon Sayo = new ImageIcon("C:\\JavaGame\\src\\com\\game\\resources\\Sayo.png");
-        public static final ImageIcon Lisa = new ImageIcon("C:\\JavaGame\\src\\com\\game\\resources\\Lisa.png");
-        public static final ImageIcon Ako = new ImageIcon("C:\\JavaGame\\src\\com\\game\\resources\\Ako.png");
-        public static final ImageIcon Rinko = new ImageIcon("C:\\JavaGame\\src\\com\\game\\resources\\Rinko.png");
+        public static final ImageIcon Yukina = new ImageIcon("src/com/game/resources/Yukina.png");
+        public static final ImageIcon Sayo = new ImageIcon("src/com/game/resources/Sayo.png");
+        public static final ImageIcon Lisa = new ImageIcon("src/com/game/resources/Lisa.png");
+        public static final ImageIcon Ako = new ImageIcon("src/com/game/resources/Ako.png");
+        public static final ImageIcon Rinko = new ImageIcon("src/com/game/resources/Rinko.png");
 
         //Hello, Happy World!
-        public static final ImageIcon Kokoru = new ImageIcon("C:\\JavaGame\\src\\com\\game\\resources\\Kokoru.png");
-        public static final ImageIcon Kaoru = new ImageIcon("C:\\JavaGame\\src\\com\\game\\resources\\Kaoru.png");
-        public static final ImageIcon Hagumi = new ImageIcon("C:\\JavaGame\\src\\com\\game\\resources\\Hagumi.png");
-        public static final ImageIcon Kanon = new ImageIcon("C:\\JavaGame\\src\\com\\game\\resources\\Kanon.png");
-        public static final ImageIcon Michelle = new ImageIcon("C:\\JavaGame\\src\\com\\game\\resources\\Michelle.png");
+        public static final ImageIcon Kokoru = new ImageIcon("src/com/game/resources/Kokoru.png");
+        public static final ImageIcon Kaoru = new ImageIcon("src/com/game/resources/Kaoru.png");
+        public static final ImageIcon Hagumi = new ImageIcon("src/com/game/resources/Hagumi.png");
+        public static final ImageIcon Kanon = new ImageIcon("src/com/game/resources/Kanon.png");
+        public static final ImageIcon Michelle = new ImageIcon("src/com/game/resources/Michelle.png");
 
         //Morfonica
-        public static final ImageIcon Mashiro = new ImageIcon("C:\\JavaGame\\src\\com\\game\\resources\\Mashiro.png");
-        public static final ImageIcon Toko = new ImageIcon("C:\\JavaGame\\src\\com\\game\\resources\\Toko.png");
-        public static final ImageIcon Nanami = new ImageIcon("C:\\JavaGame\\src\\com\\game\\resources\\Nanami.png");
-        public static final ImageIcon Tsukushi = new ImageIcon("C:\\JavaGame\\src\\com\\game\\resources\\Tsukushi.png");
-        public static final ImageIcon Rui = new ImageIcon("C:\\JavaGame\\src\\com\\game\\resources\\Rui.png");
+        public static final ImageIcon Mashiro = new ImageIcon("src/com/game/resources/Mashiro.png");
+        public static final ImageIcon Toko = new ImageIcon("src/com/game/resources/Toko.png");
+        public static final ImageIcon Nanami = new ImageIcon("src/com/game/resources/Nanami.png");
+        public static final ImageIcon Tsukushi = new ImageIcon("src/com/game/resources/Tsukushi.png");
+        public static final ImageIcon Rui = new ImageIcon("src/com/game/resources/Rui.png");
 
         //Raise A Suilen
-        public static final ImageIcon Layer = new ImageIcon("C:\\JavaGame\\src\\com\\game\\resources\\Layer.png");
-        public static final ImageIcon Lock = new ImageIcon("C:\\JavaGame\\src\\com\\game\\resources\\Lock.png");
-        public static final ImageIcon Masking = new ImageIcon("C:\\JavaGame\\src\\com\\game\\resources\\Masking.png");
-        public static final ImageIcon Pareo = new ImageIcon("C:\\JavaGame\\src\\com\\game\\resources\\Pareo.png");
-        public static final ImageIcon Chuchu = new ImageIcon("C:\\JavaGame\\src\\com\\game\\resources\\Chuchu.png");
+        public static final ImageIcon Layer = new ImageIcon("src/com/game/resources/Layer.png");
+        public static final ImageIcon Lock = new ImageIcon("src/com/game/resources/Lock.png");
+        public static final ImageIcon Masking = new ImageIcon("src/com/game/resources/Masking.png");
+        public static final ImageIcon Pareo = new ImageIcon("src/com/game/resources/Pareo.png");
+        public static final ImageIcon Chuchu = new ImageIcon("src/com/game/resources/Chuchu.png");
 
         //Mygo!!!!!
-        public static final ImageIcon Anon = new ImageIcon("C:\\JavaGame\\src\\com\\game\\resources\\Anon.png");
-        public static final ImageIcon Tomori = new ImageIcon("C:\\JavaGame\\src\\com\\game\\resources\\Tomori.png");
-        public static final ImageIcon Soyo = new ImageIcon("C:\\JavaGame\\src\\com\\game\\resources\\Soyo.png");
-        public static final ImageIcon Rana = new ImageIcon("C:\\JavaGame\\src\\com\\game\\resources\\Rana.png");
-        public static final ImageIcon Taki = new ImageIcon("C:\\JavaGame\\src\\com\\game\\resources\\Taki.png");
+        public static final ImageIcon Anon = new ImageIcon("/com/game/resources/Anon.png");
+        public static final ImageIcon Tomori = new ImageIcon("/com/game/resources/Tomori.png");
+        public static final ImageIcon Soyo = new ImageIcon("/com/game/resources/Soyo.png");
+        public static final ImageIcon Rana = new ImageIcon("/com/game/resources/Rana.png");
+        public static final ImageIcon Taki = new ImageIcon("/com/game/resources/Taki.png");
 
 
-        public static final ArrayList<ImageIcon> ppp = new ArrayList<>(List.of(Kasumi, Tae, Rimi, Saya, Arisa));;
+        public static final ArrayList<ImageIcon> ppp = new ArrayList<>(List.of(Kasumi, Tae, Rimi, Saya, Arisa));
         public static final ArrayList<ImageIcon> ag = new ArrayList<>(List.of(Ran, Moca, Himari, Tomoe, Tsugumi));
         public static final ArrayList<ImageIcon> pp = new ArrayList<>(List.of(Aya, Hina, Chisato, Maya, Eve));
         public static final ArrayList<ImageIcon> ros = new ArrayList<>(List.of(Yukina, Sayo, Lisa, Ako, Rinko));
@@ -72,7 +74,7 @@ public class Characters {
         public static final ArrayList<ImageIcon> mor = new ArrayList<>(List.of(Mashiro, Toko, Nanami, Tsukushi, Rui));
         public static final ArrayList<ImageIcon> ras = new ArrayList<>(List.of(Layer, Lock, Masking, Pareo, Chuchu));
         public static final ArrayList<ImageIcon> mygo = new ArrayList<>(List.of(Anon, Tomori, Soyo, Rana, Taki));
-        public static final ArrayList<ImageIcon> _ppp = new ArrayList<>(List.of(Kasumi, Tae, Rimi, Saya, Arisa));;
+        public static final ArrayList<ImageIcon> _ppp = new ArrayList<>(List.of(Kasumi, Tae, Rimi, Saya, Arisa));
         public static final ArrayList<ImageIcon> _ag = new ArrayList<>(List.of(Ran, Moca, Himari, Tomoe, Tsugumi));
         public static final ArrayList<ImageIcon> _pp = new ArrayList<>(List.of(Aya, Hina, Chisato, Maya, Eve));
         public static final ArrayList<ImageIcon> _ros = new ArrayList<>(List.of(Yukina, Sayo, Lisa, Ako, Rinko));
@@ -87,13 +89,13 @@ public class Characters {
 
 
 
-        public static final ImageIcon ppp_ = new ImageIcon("C:\\JavaGame\\src\\com\\game\\resources\\ppp.png");
-        public static final ImageIcon ag_ = new ImageIcon("C:\\JavaGame\\src\\com\\game\\resources\\ag.png");
-        public static final ImageIcon pp_ = new ImageIcon("C:\\JavaGame\\src\\com\\game\\resources\\pp.png");
-        public static final ImageIcon ros_ = new ImageIcon("C:\\JavaGame\\src\\com\\game\\resources\\ros.png");
-        public static final ImageIcon hhw_ = new ImageIcon("C:\\JavaGame\\src\\com\\game\\resources\\hhw.png");
-        public static final ImageIcon mor_ = new ImageIcon("C:\\JavaGame\\src\\com\\game\\resources\\mor.png");
-        public static final ImageIcon ras_ = new ImageIcon("C:\\JavaGame\\src\\com\\game\\resources\\ras.png");
-        public static final ImageIcon mygo_ = new ImageIcon("C:\\JavaGame\\src\\com\\game\\resources\\mygo.png");
+        public static final ImageIcon ppp_ = new ImageIcon("src/com/game/resources/ppp.png");
+        public static final ImageIcon ag_ = new ImageIcon("src/com/game/resources/ag.png");
+        public static final ImageIcon pp_ = new ImageIcon("src/com/game/resources/pp.png");
+        public static final ImageIcon ros_ = new ImageIcon("src/com/game/resources/ros.png");
+        public static final ImageIcon hhw_ = new ImageIcon("src/com/game/resources/hhw.png");
+        public static final ImageIcon mor_ = new ImageIcon("src/com/game/resources/mor.png");
+        public static final ImageIcon ras_ = new ImageIcon("src/com/game/resources/ras.png");
+        public static final ImageIcon mygo_ = new ImageIcon("/com/game/resources/mygo.png");
         public static final ArrayList<ImageIcon> bands = new ArrayList<>(List.of(ppp_, ag_, pp_, ros_, hhw_, mor_, ras_));
 }
